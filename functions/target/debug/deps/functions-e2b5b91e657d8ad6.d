49464b5b6a0b11011ae8f9bbd2d0862a177dce32
Takahiro Hara <takahiro.hb@gmail.com>
2022-01-26 18:32:31 +0900

    init functions dir

---
+++ functions/target/debug/deps/functions-e2b5b91e657d8ad6.d
@@ -0,0 +1,5 @@
+/Users/takahiro_hara/repos/rust_sample/functions/target/debug/deps/functions-e2b5b91e657d8ad6: src/main.rs
+
+/Users/takahiro_hara/repos/rust_sample/functions/target/debug/deps/functions-e2b5b91e657d8ad6.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/Users/takahiro_hara/repos/rust_sample/functions/target/debug/functions: /Users/takahiro_hara/repos/rust_sample/functions/src/main.rs
