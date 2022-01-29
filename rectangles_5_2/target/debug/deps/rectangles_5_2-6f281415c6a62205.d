a572260484e76a2917295a081072d25dbfc13745
Takahiro Hara <takahiro.hb@gmail.com>
2022-01-30 01:05:15 +0900

    update rectangles_5_2

---
+++ rectangles_5_2/target/debug/deps/rectangles_5_2-6f281415c6a62205.d
@@ -0,0 +1,5 @@
+/Users/takahiro_hara/repos/rust_sample/rectangles_5_2/target/debug/deps/rectangles_5_2-6f281415c6a62205: src/main.rs
+
+/Users/takahiro_hara/repos/rust_sample/rectangles_5_2/target/debug/deps/rectangles_5_2-6f281415c6a62205.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles_5_2/target/debug/rectangles_5_2.d
@@ -0,0 +1,1 @@
+/Users/takahiro_hara/repos/rust_sample/rectangles_5_2/target/debug/rectangles_5_2: /Users/takahiro_hara/repos/rust_sample/rectangles_5_2/src/main.rs
