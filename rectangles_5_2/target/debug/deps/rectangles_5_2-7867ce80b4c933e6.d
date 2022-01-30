b97f2e2bb1ea394941ba715442317cbd57716a3e
Takahiro Hara <takahiro.hb@gmail.com>
2022-01-30 17:54:27 +0900

    update rectangle_5_2: test associated function

---
+++ rectangles_5_2/target/debug/deps/rectangles_5_2-7867ce80b4c933e6.d
@@ -0,0 +1,5 @@
+/Users/takahiro_hara/repos/rust_sample/rectangles_5_2/target/debug/deps/rectangles_5_2-7867ce80b4c933e6.rmeta: src/main.rs
+
+/Users/takahiro_hara/repos/rust_sample/rectangles_5_2/target/debug/deps/rectangles_5_2-7867ce80b4c933e6.d: src/main.rs
+
+src/main.rs:
